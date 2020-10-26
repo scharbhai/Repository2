631045dd95f3920b473cd58920c6b529e4211f7f
Amol <amol.charbhai@gmail.com>
2020-10-26 16:07:12 +0530

    commit new

---
+++ src/test/java/regressionTest/VerifyTitle.java
@@ -0,0 +1,24 @@
+package regressionTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class VerifyTitle {
+
+	@Test(retryAnalyzer = RetryAnalyzer.class)
+	public void testCorrectTitle() throws InterruptedException  {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://Amazon.com");
+
+		Assert.assertTrue(driver.getTitle().contains("AMAZON"));
+		driver.quit();
+		
+
+	}
+
+}
