5d2fded228698bd99bbd0ba05079990eaa5477e2
Amol <amol.charbhai@gmail.com>
2020-10-29 15:41:20 +0530

    comments updated in Retry Test

--- src/test/java/regressionTest/RetryAnalyzer.java
+++ src/test/java/regressionTest/RetryAnalyzer.java
@@ -13,9 +13,9 @@ public class RetryAnalyzer implements IRetryAnalyzer {
  
     @Override
     public boolean retry(ITestResult iTestResult) {
-        if (!iTestResult.isSuccess()) {                      //Check if test not success
-            if (count < maxTry) {                            //Check if maxtry count is reached
-                count++;                                     //Increase the maxTry count by 1
+        if (!iTestResult.isSuccess()) {                     
+            if (count < maxTry) {                            
+                count++;                                    
                 iTestResult.setStatus(ITestResult.FAILURE);  //Mark test as failed
              
                 return true;                                 //Tells TestNG to re-run the test
