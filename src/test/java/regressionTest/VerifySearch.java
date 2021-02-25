9c90bdcc765198220f283e52a41cd3b5d226feb7
Amol <amol.charbhai@gmail.com>
2021-02-25 12:02:37 +0530

    new test

---
+++ src/test/java/regressionTest/VerifySearch.java
@@ -0,0 +1,36 @@
+/*
+ * Author: Shailaja Charbhai
+ * new date
+ */
+
+package regressionTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class VerifySearch {
+
+	//This Test verifies the Search functionality  
+	@Test
+	public void testVerifyTitle() throws InterruptedException  {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.lntinfotech.com/");
+		Thread.sleep(1000);
+		Assert.assertTrue(driver.getTitle().contains("LTI"));
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//img[@alt='Search']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//input[@name='search-text']")).sendKeys("LTI");
+		Thread.sleep(1000);
+		driver.quit();
+		
+
+	}
+
+}
